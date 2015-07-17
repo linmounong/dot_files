d4bfbd3c24bc4f74067842206f4868a48eb1e86a
linmounong <linmounong@gmail.com>
2015-07-16 23:03:32 -0700

    ADD remapmenu.sh

--- onetime/git.sh
+++ onetime/git.sh
@@ -1,2 +1,4 @@
 git config --global user.email 'linmounong@gmail.com'
 git config --global user.name 'linmounong'
+git config --global credential.helper 'cache --timeout=86400'
+git config --global push.default simple
---
+++ onetime/remapmenu.sh
@@ -0,0 +1,2 @@
+sudo sed 's/Menu/Shift_L/' /usr/share/X11/xkb/symbols/pc -i
+sudo rm -rf /var/lib/xkb/*

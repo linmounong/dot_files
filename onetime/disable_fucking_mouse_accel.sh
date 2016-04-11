c5576446f1a947150f0ea6f37a1ab2cced62a2e3
linmounong <linmounong@gmail.com>
2016-04-12 00:08:05 +0800

    Disabled the f**king mouse accel

---
+++ onetime/disable_fucking_mouse_accel.sh
@@ -0,0 +1,10 @@
+set -x
+
+#gets the hardware id's of all mice plugged into the system
+hardwareIds=$(xinput | grep -i logitech | awk '{print substr($(NF-3),4)}')
+
+#turn off mouse acceleration
+for i in $hardwareIds; do
+  xinput set-prop ${i} 'Device Accel Profile' -1
+  xinput set-prop ${i} 'Device Accel Velocity Scaling' 1
+done

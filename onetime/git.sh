e33289b7a96e6b8fe17dd9d9d31c096982703ffb
linmounong <linmounong@gmail.com>
2016-05-08 15:19:19 +0800

    Deleted unused git scripts

--- onetime/git.sh
+++
@@ -1,4 +0,0 @@
-git config --global user.email 'linmounong@gmail.com'
-git config --global user.name 'linmounong'
-git config --global credential.helper 'cache --timeout=86400'
-git config --global push.default simple

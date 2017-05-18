5bd2813e8c47d894714388a375bd8acdee9769e4
linmounong <linmounong@gmail.com>
2017-05-18 17:21:26 +0800

    Fixed tmux conf path.

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,4 +1,4 @@
-ln -s $HOME/git/dot_files/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/git/dot_files/tmux/tmux.conf $HOME/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm $HOME/tmux/plugins/tpm
 echo "run:"
 echo "  sudo apt-get install libevent-dev libncurses-dev"

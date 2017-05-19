98d14b958a1b2c598e6d06488b525e297f213db2
linmounong <linmounong@gmail.com>
2017-05-19 12:40:49 +0800

    install-tmux.sh

---
+++ tmux/install-tmux.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Exit on error #
+set -e
+
+WORKDIR=~/tmuxtmp
+TMUX_VERSION=2.2
+
+# Clean up #
+# Create our directories #
+cleanup() {
+  rm -rf $WORKDIR/libevent
+  rm -rf $WORKDIR/ncurses
+  rm -rf $WORKDIR/tmux
+  mkdir -p $WORKDIR/test
+  mkdir -p $WORKDIR/libevent
+  mkdir -p $WORKDIR/ncurses
+  mkdir -p $WORKDIR/tmux
+}
+
+############
+# libevent #
+############
+do_libevent() {
+  cd $WORKDIR/test
+  curl -LO https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
+  tar xvzf libevent-2.0.19-stable.tar.gz
+  cd libevent-*/
+  ./configure --prefix=$WORKDIR/libevent --disable-shared
+  make
+  make install
+}
+
+############
+# ncurses  #
+############
+do_ncurses() {
+  cd $WORKDIR/test
+  wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
+  tar xvzf ncurses-5.9.tar.gz
+  cd ncurses-5.9
+  ./configure --prefix=$WORKDIR/ncurses LDFLAGS="-static"
+  make
+  make install
+}
+
+############
+# tmux     #
+############
+do_tmux() {
+  cd $WORKDIR/test
+  # wget -O tmux-${TMUX_VERSION}.tar.gz http://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+  # tar xvzf tmux-${TMUX_VERSION}.tar.gz
+  cd tmux-${TMUX_VERSION}
+
+  # open configure and find the line that says:
+  # PKG_CONFIG="pkg-config --static"
+  # And comment it out:
+  # #PKG_CONFIG="pkg-config --static"
+  sed -i.bak 's/PKG_CONFIG="pkg-config --static"/# \0/' configure
+
+  # Build #
+  CFLAGS="-I$WORKDIR/libevent/include -I$WORKDIR/ncurses/include" \
+  LDFLAGS="-static -L$WORKDIR/libevent/lib -L$WORKDIR/libevent/include -L$WORKDIR/ncurses/lib -L$WORKDIR/ncurses/include" \
+  PKG_CONFIG=/bin/false \
+    ./configure \
+    --prefix=$WORKDIR/tmux \
+    --enable-static
+
+  CPPFLAGS="-I$WORKDIR/libevent/include -I$WORKDIR/ncurses/include" \
+  LDFLAGS="-static -L$WORKDIR/libevent/lib -L$WORKDIR/libevent/include -L$WORKDIR/ncurses/lib -L$WORKDIR/ncurses/include" \
+    make
+
+  cp tmux ~/bin/tmux
+}
+
+do_tmux
--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,6 +1,7 @@
 ln -s $HOME/git/dot_files/tmux/tmux.conf $HOME/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm $HOME/tmux/plugins/tpm
-echo "run:"
-echo "  sudo apt-get install libevent-dev libncurses-dev"
-echo "download from https://tmux.github.io/"
-echo "run tmux and then ctrl+I"
+echo "1) if you have sudo, run:"
+echo "    sudo apt-get install libevent-dev libncurses-dev"
+echo "    download tmux from https://tmux.github.io/"
+echo "  otherwise run ./install-tmux.sh"
+echo "2) run tmux and then ctrl+I"

3befa6b5eb7fd2e15cfe329b3269e6202fcbf78f
Yinong Lin <linmounong@gmail.com>
2017-05-24 15:29:18 +0800

    vim: replaced vim-clangformat with google's vim-codefmt

--- tools/mount_sshfs.sh
+++ tools/mount_sshfs.sh
@@ -1,1 +1,2 @@
 sudo sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,allow_other,defer_permissions,IdentityFile=~/.ssh/v ynlin@v:/home/ynlin /mnt/v
+sudo sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,allow_other,defer_permissions,IdentityFile=~/.ssh/u ynlin@u:/home/ynlin /mnt/u
--- vim/setup.sh
+++ vim/setup.sh
@@ -3,10 +3,14 @@ sduo ln -s /usr/bin/clang-format-3.8 /usr/bin/clang-format
 ln -s ~/git/dot_files/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/indent
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 cd ~/.vim/bundle
 git clone https://github.com/ctrlpvim/ctrlp.vim
 git clone https://github.com/junegunn/vim-easy-align
 git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/rhysd/vim-clang-format
+git clone https://github.com/google/vim-maktaba.git  # used by codefmt
+git clone https://github.com/google/vim-glaive.git  # used by codefmt
+git clone https://github.com/google/vim-codefmt.git
+
 cd ~/.vim/indent
 wget http://www.vim.org/scripts/download_script.php?src_id=14772 -O cpp.vim

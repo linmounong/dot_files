accd725e55fcfa54518753725bf93afb4d6f411f
linmounong <linmounong@gmail.com>
2018-07-19 05:52:28 +0800

    script to install oracle java8

---
+++ install_java.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -ex
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
